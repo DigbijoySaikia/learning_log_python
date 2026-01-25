0c9823d16c2c6edd5c42445823704561ea5e6f0f
digbijoysaikia <ytdigbijoy123@gmail.com>
2026-01-25 22:11:44 +0530

    day 1

---
+++ day1/if_else.py
@@ -0,0 +1,7 @@
+a = input("enter 1st number")
+b = input ("enter 2nd number")
+
+if a > b:
+    print("first number is greater than second number")
+if a < b :
+    print("first number is smaller than second number")
